b37c9ec97c91fab25b35d413d70d4c9e6e13cf95
Gusev Timofey <ioff205@gmail.com>
2022-05-14 23:36:09 +0700

    update .github

--- src/libgeom/geometry.h
+++ src/libgeom/geometry.h
@@ -3,30 +3,27 @@
 
 using namespace std;
 
-struct Point
-{
-	double x;
-	double y;
+struct Point {
+    double x;
+    double y;
 };
 
-struct Circle
-{
-	int Order;
-	Point Center;
-	double r;
-	double P;
-	double S;
+struct Circle {
+    int Order;
+    Point Center;
+    double r;
+    double P;
+    double S;
 };
 
-struct Triangle
-{
-	int Order;
-	Point A;
-	Point B;
-	Point C;
-	Point End;
-	double P;
-	double S;
+struct Triangle {
+    int Order;
+    Point A;
+    Point B;
+    Point C;
+    Point End;
+    double P;
+    double S;
 };
 
 Point Point_analysis(int string, std::string storage[]);
@@ -36,8 +33,10 @@ Triangle Triangle_analysis(int order, int string, std::string storage[]);
 int Intersec_T(Point A, Point B, Point C, Point D);
 int Intersec_C(Point A, Point B, Point C, float r);
 
-void Circle_intersec(Circle C, Circle CM[], int size_CM, Triangle TM[], int size_TM);
-void Triangle_intersec(Triangle T, Circle CM[], int size_CM, Triangle TM[], int size_TM);
+void Circle_intersec(
+        Circle C, Circle CM[], int size_CM, Triangle TM[], int size_TM);
+void Triangle_intersec(
+        Triangle T, Circle CM[], int size_CM, Triangle TM[], int size_TM);
 
 void Circle_print(Circle C, std::string storage[]);
 void Triangle_print(Triangle T, std::string storage[]);
